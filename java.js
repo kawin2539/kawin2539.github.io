0faf6ac17478266bec08fcd35ecdc5635c919f9f
Kawin wanthongkum <2610711342001@live4.utcc.ac.th>
2026-09-13 15:32:12 +0700

    Update PDF URL in openPresentationPDF function

--- java.js
+++ java.js
@@ -1,6 +1,6 @@
 function openPresentationPDF() {
   // 1. ระบุ URL หรือ Path ของไฟล์ PDF ที่ต้องการเชื่อมโยง
-  const pdfUrl = 'presentation.pdf'; // เปลี่ยน 'presentation.pdf' เป็นชื่อไฟล์ หรือ URL ของไฟล์ PDF จริง
+  const pdfUrl = '1lxk89E2N7Cm5kFxCphGqcuNHhDQ1HDsQ'; // เปลี่ยน 'presentation.pdf' เป็นชื่อไฟล์ หรือ URL ของไฟล์ PDF จริง
 
   // 2. เปิดไฟล์ PDF ในหน้าต่าง/แท็บใหม่
   window.open(pdfUrl, '_blank');
